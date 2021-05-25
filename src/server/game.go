b85b4ab65202a82f691eb1ed5c3678cb4396d0a5
Puja Kusuma <pujakusumae@hotmail.com>
2021-05-25 10:21:29 +0700

    Refactor websocket game

--- src/server/game.go
+++ src/server/game.go
@@ -5,8 +5,10 @@ import (
 	"time"
 )
 
+const TimeUnit = time.Second
+
 type Game interface {
-	Start(numOfPlayers int, alertDestination io.Writer)
+	Start(numOfPlayers int, out io.Writer)
 	Finish(winner string)
 }
 
@@ -19,13 +21,13 @@ func NewTexasHoldem(alerter BlindAlerter, store PlayerStore) *TexasHoldem {
 	return &TexasHoldem{alerter: alerter, store: store}
 }
 
-func (g *TexasHoldem) Start(numOfPlayers int, alertDestination io.Writer) {
-	blindInc := time.Duration(5+numOfPlayers) * time.Second //time.Minute
+func (g *TexasHoldem) Start(numOfPlayers int, out io.Writer) {
+	blindInc := time.Duration(5+numOfPlayers) * TimeUnit
 
 	blinds := []int{100, 200, 300, 400, 500, 600, 800, 1000, 2000, 4000, 8000}
-	blindTime := 0 * time.Second
+	blindTime := 0 * TimeUnit
 	for _, blind := range blinds {
-		g.alerter.ScheduleAlertAt(blindTime, blind, alertDestination)
+		g.alerter.ScheduleAlertAt(blindTime, blind, out)
 		blindTime += blindInc
 	}
 }
--- src/server/server_test.go
+++ src/server/server_test.go
@@ -64,7 +64,7 @@ func TestStoreWins(t *testing.T) {
 		server.ServeHTTP(res, req)
 
 		assertStatus(t, res, http.StatusAccepted)
-		poker.AssertPlayerWin(t, store, player)
+		assertPlayerWin(t, store, player)
 	})
 }
 
@@ -116,9 +116,11 @@ func TestGame(t *testing.T) {
 		assertStatus(t, res, http.StatusOK)
 	})
 
-	t.Run("start a game with 3 players and declare 'Ruth' the winner", func(t *testing.T) {
-		game := &poker.SpyGame{}
+	t.Run("start a game with 3 players, send blindAlert to ws, and declare 'Ruth' as winner", func(t *testing.T) {
+		wantedBlindAlert := "Blind is 100"
 		winner := "Ruth"
+
+		game := &poker.SpyGame{BlindAlert: []byte(wantedBlindAlert)}
 
 		server := httptest.NewServer(mustMakePlayerServer(t, dummyPlayerStore, game))
 		ws := mustDialWS(t, "ws"+strings.TrimPrefix(server.URL, "http")+"/ws")
@@ -129,9 +131,44 @@ func TestGame(t *testing.T) {
 		writeWSMessage(t, ws, winner)
 
 		time.Sleep(10 * time.Millisecond)
-		poker.AssertGameStartedWith(t, game, 3)
-		poker.AssertFinishCalledWith(t, game, winner)
+		assertGameStartedWith(t, game, 3)
+		assertFinishCalledWith(t, game, winner)
+		within(t, 10*time.Millisecond, func() {
+			assertWSMessage(t, ws, wantedBlindAlert)
+		})
 	})
+}
+
+func assertFinishCalledWith(t *testing.T, game *poker.SpyGame, winner string) {
+	t.Helper()
+
+	passed := retryUntil(500*time.Millisecond, func() bool {
+		return game.FinishCalledWith == winner
+	})
+
+	if !passed {
+		t.Errorf(`it finished with %q winner, want %q`, game.FinishCalledWith, winner)
+	}
+}
+
+func assertGameStartedWith(t *testing.T, game *poker.SpyGame, numOfPlayers int) {
+	t.Helper()
+
+	passed := retryUntil(500*time.Millisecond, func() bool {
+		return game.StartCalledWith == numOfPlayers
+	})
+
+	if !passed {
+		t.Errorf(`it started with %d players, want %d`, game.StartCalledWith, numOfPlayers)
+	}
+}
+
+func assertWSMessage(t *testing.T, ws *websocket.Conn, wantedMsg string) {
+	t.Helper()
+	_, gotMsg, _ := ws.ReadMessage()
+	if string(gotMsg) != wantedMsg {
+		t.Errorf("got blind alert %q, wanted %q", gotMsg, wantedMsg)
+	}
 }
 
 func assertContentType(t *testing.T, res *httptest.ResponseRecorder, want string) {
@@ -211,4 +248,30 @@ func writeWSMessage(t *testing.T, ws *websocket.Conn, msg string) {
 	if err := ws.WriteMessage(websocket.TextMessage, []byte(msg)); err != nil {
 		t.Fatalf("can't send msg via ws connection %v", err)
 	}
+}
+
+func within(t testing.TB, d time.Duration, assert func()) {
+	t.Helper()
+
+	done := make(chan struct{}, 1)
+	go func() {
+		assert()
+		done <- struct{}{}
+	}()
+
+	select {
+	case <-time.After(d):
+		t.Error("timed out")
+	case <-done:
+	}
+}
+
+func retryUntil(d time.Duration, fn func() bool) bool {
+	deadline := time.Now().Add(d)
+	for time.Now().Before(deadline) {
+		if fn() {
+			return true
+		}
+	}
+	return false
 }
