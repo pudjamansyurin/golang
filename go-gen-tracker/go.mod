dd971f47a83f59c00086c987c8206da68a292f8d
Puja Kusuma <pujakusumae@hotmail.com>
2021-07-01 08:16:35 +0700

    Separate go-gen-tracker project

--- go-gen-tracker/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/pudjamansyurin/go-gen-tracker
-
-go 1.16
-
-require github.com/eclipse/paho.mqtt.golang v1.3.5
--- go-gen-tracker/packet/header.go
+++
@@ -1,8 +0,0 @@
-package packet
-
-type HeaderPacket struct {
-	Prefix       string `type:"byte" len:"2"`
-	Size         uint8  `type:"uint8" unit:"Bytes" chartable:""`
-	Vin          uint32 `type:"uint32"`
-	SendDatetime int64  `type:"byte" len:"7" chartable:""`
-}
